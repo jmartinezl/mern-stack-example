4f832d934568e735264e3709a82b25b0d7892fa9
jmartinezl <jmartinezl@192.168.1.6>
2022-04-17 21:54:08 -0500

    innit

---
+++ server/server.js
@@ -0,0 +1,34 @@
+// Our DB Configuration
+require('./src/database');
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.get('/', (req, res) => {
+    res.send("Hello World ! ");
+});
+
+app.listen(PORT, function () {
+    console.log(`Server Listening on ${PORT}`);
+});
+
+const bodyParser = require('body-parser');
+
+// Routes
+const postRouter = require('./src/routes/post.router');
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(bodyParser.json());
+
+app.use('/posts', postRouter);
+
+router = express.Router();
+
+// will redirect all the non-api routes to react frontend
+router.use(function(req, res) {
+    res.sendFile(path.join(__dirname, '../client','build','index.html'));
+});
